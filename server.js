d70cde8a467363b9ebec35fa0194090cb26a1bec
riandias1331 <@galoucura321>
2024-12-15 11:10:00 -0300

    teste

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const path = require('path')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const session = require('express-session') 
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash') 
+
+const sessionOptions = session({
+    secret: 'askodjiyfdygdlm', 
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }), 
+    resave: false, 
+    saveUninitialized: true, 
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, 
+        httpOnly: true 
+    }
+})
+
+app.use(sessionOptions) // aplica as opções de sessão configuradas
+app.use(flash()) // ativa o uso de flash messages
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.CONNECTION)
     .then(() => {
